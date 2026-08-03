fbe7a7199edf7af07d9a9d5cbb65e654647384e2
Darshan Dalwadi <dalwadidarshan1010@gmail.com>
2026-08-03 19:43:40 +0530

    chore: ignore strict any and unused vars rules for igg-ai prototype

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["igg-ai/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  }
 ]);
 
 export default eslintConfig;
